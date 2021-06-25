90c77ab909bbb1419399ebe251430825842b6493
joeldevel <christianjoel.developer@gmail.com>
2021-06-24 22:55:22 -0300

    exercises, objects, book constructor

---
+++ objects/books.js
@@ -0,0 +1,13 @@
+function Book(title, author, pages, read ) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  let readStatus = this.read ? 'already read' : 'not read yet';
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${pages} pages, ${readStatus}.`;
+  }
+}
+let book1 = new Book('Ivy Gripped the Steps', 'Elizabeth Bowen', 23, true);
+
+console.log(book1.info());
