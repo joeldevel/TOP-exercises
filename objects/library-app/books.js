dd9a83bb42511d8b4238bc9579eceea44a23efc8
joeldevel <christianjoel.developer@gmail.com>
2021-07-29 01:13:04 -0300

    can add books to library

--- objects/library-app/books.js
+++ objects/library-app/books.js
@@ -1,10 +1,13 @@
 
 //get inputs
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const pages = document.querySelector("#pages");
-const read = document.getElementsByName("read");
-console.log(title, author, pages);
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const read = document.getElementsByName('read');
+
+const createNewBook = document.querySelector('#create-new-book');
+console.log(createNewBook);
+// console.log(title.value, author, pages);
 
 /*
 for (let i of read) {
@@ -14,7 +17,12 @@ if (i.checked) {
   }
 }
 */
-
+function checkRead(input) {
+  for (let i of input) {
+    if (i.checked) return i;
+  }
+}
+// console.log(checkRead(read).value);
 let myLibrary = [];
 
 function Book(title, author, pages, read ) {
@@ -28,13 +36,39 @@ function Book(title, author, pages, read ) {
   }
 }
 
-function addBookToLibrary(book, library) {
-
+function addBookToLibrary(e) {
+  e.preventDefault();
+  const book = new Book(title.value, author.value, parseInt(pages.value), true);
+  myLibrary.push(book);
+  console.log("insertando libro");
+  appendBookToTable(book);
 }
 
 function displayBooks(books) {
+  books.forEach(appendBookToTable);
+}
 
+function appendBookToTable(book) {
+  let table = document.querySelector('.display-books');
+  const row = `<tr>
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.pages}</td>
+      <td>${book.read}</td>
+  </tr>`;
+  table.insertAdjacentHTML("beforeend", row);
 }
 let book1 = new Book('Ivy Gripped the Steps', 'Elizabeth Bowen', 23, true);
+let book2 = new Book('A Good Investment', 'Joyce Cary', 14, false);
+let book3 = new Book('The Killing Bottle', 'L. P. Hartley', 40, true);
+let book4 = new Book('The Scapegoat', 'V. S. Pritchett', 38, false);
 
-console.log(book1.info());
+myLibrary.push(book1);
+myLibrary.push(book2);
+myLibrary.push(book3);
+myLibrary.push(book4);
+
+displayBooks(myLibrary);
+
+createNewBook.addEventListener('click', addBookToLibrary);
+// console.log(book1.info());
