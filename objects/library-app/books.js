3a02125e203411f2e62909b2687340aa3711f9d4
joeldevel <christianjoel.developer@gmail.com>
2021-07-29 18:38:13 -0300

    library-app can create and delete books and toggle read status

--- objects/library-app/books.js
+++ objects/library-app/books.js
@@ -5,23 +5,27 @@ const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const read = document.getElementsByName('read');
 
+const table = document.querySelector('.display-books');
 const createNewBook = document.querySelector('#create-new-book');
-console.log(createNewBook);
+
+
+// console.log(createNewBook);
 // console.log(title.value, author, pages);
 
 /*
 for (let i of read) {
-
 if (i.checked) {
     console.log(i);
   }
 }
 */
+
 function checkRead(input) {
   for (let i of input) {
     if (i.checked) return i;
   }
 }
+
 // console.log(checkRead(read).value);
 let myLibrary = [];
 
@@ -48,16 +52,50 @@ function displayBooks(books) {
   books.forEach(appendBookToTable);
 }
 
+// let index = myArray.findIndex( element => {
+//   if (element.name === 'Maria') {
+//     return true;
+//   }
+// });
+function findBookIndex(title) {
+  let index = myLibrary.findIndex( element => {
+    if (element.title === title) {
+      return true;
+    }
+  });
+  return index;
+}
+
 function appendBookToTable(book) {
-  let table = document.querySelector('.display-books');
-  const row = `<tr>
+  const row = `<tr data-book-id="${findBookIndex(book.title)}">
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td>${book.pages}</td>
-      <td>${book.read?'yes':'no'}</td>
+      <td><button type="button" name="toggle-read-button">${book.read?'yes':'no'}</button></td>
+      <td><button type="button" name="delete-button">delete</button></td>
   </tr>`;
   table.insertAdjacentHTML("beforeend", row);
 }
+
+function deleteBookFromTable(bookId) {
+  const row = document.querySelector(`[data-book-id="${bookId}"]`);
+  console.log(row);
+  table.removeChild(row);
+
+}
+
+function updateReadStatus(bookId) {
+  myLibrary[bookId].toggleReadStatus();
+}
+
+// function getBookInTable() {
+//
+// }
+
+//********************************************************************/
+//                          populate books
+//********************************************************************/
+
 let book1 = new Book('Ivy Gripped the Steps', 'Elizabeth Bowen', 23, true);
 let book2 = new Book('A Good Investment', 'Joyce Cary', 14, false);
 let book3 = new Book('The Killing Bottle', 'L. P. Hartley', 40, true);
@@ -70,5 +108,52 @@ myLibrary.push(book4);
 
 displayBooks(myLibrary);
 
+//********************************************************************/
+//                    event listeners
+//********************************************************************/
+
 createNewBook.addEventListener('click', addBookToLibrary);
+
+// add event listener to dinamically generated elements
+document.querySelector('body').addEventListener('click', function(event) {
+  if (event.target.name.toLowerCase() === 'delete-button') {
+    // console.log(event.target);
+    // console.log("parent: ", event.target.parentNode);
+    // console.log("row: ", event.target.parentNode.parentNode);
+    const row = event.target.parentNode.parentNode;
+    const id = row.getAttribute("data-book-id");
+    // console.log(id.getAttribute("data-book-id"));
+    deleteBookFromTable(id);
+  }
+  if( event.target.name.toLowerCase() === 'toggle-read-button') {
+    // console.log("toggling");
+    const row = event.target.parentNode.parentNode;
+    const id = row.getAttribute("data-book-id");
+    // console.log(id);
+    // change data in myLibrary
+    updateReadStatus(id);
+
+    // update GUI
+    const button = row.querySelector(`[name="toggle-read-button"]`);
+    if(button.textContent==='yes') {
+      button.textContent = 'no';
+    }
+    else if(button.textContent==='no') {
+      button.textContent = 'yes';
+    }
+    // console.log(button.textContent);
+  }
+});
+
+
+// add functionality to book
+function toggleReadStatus() {
+  this.read = !this.read;
+}
+Book.prototype.toggleReadStatus = toggleReadStatus;
+
 // console.log(book1.info());
+
+// Student.prototype.sayName = function() {
+//   console.log(this.name)
+// }
