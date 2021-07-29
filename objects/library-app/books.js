a18a6d44e9f71ab33b6ac57523c9d0c71de824e8
joeldevel <christianjoel.developer@gmail.com>
2021-07-29 00:30:40 -0300

    library-app basic GUI

--- objects/library-app/books.js
+++ objects/library-app/books.js
@@ -1,3 +1,20 @@
+
+//get inputs
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const read = document.getElementsByName("read");
+console.log(title, author, pages);
+
+/*
+for (let i of read) {
+
+if (i.checked) {
+    console.log(i);
+  }
+}
+*/
+
 let myLibrary = [];
 
 function Book(title, author, pages, read ) {
@@ -18,6 +35,6 @@ function addBookToLibrary(book, library) {
 function displayBooks(books) {
 
 }
-// let book1 = new Book('Ivy Gripped the Steps', 'Elizabeth Bowen', 23, true);
+let book1 = new Book('Ivy Gripped the Steps', 'Elizabeth Bowen', 23, true);
 
-// console.log(book1.info());
+console.log(book1.info());
