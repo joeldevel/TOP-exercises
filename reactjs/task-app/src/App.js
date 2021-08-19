5bfdc3b0879cf52f1c48bc60327eba19065494e5
joeldevel <christianjoel.developer@gmail.com>
2021-08-19 02:49:25 -0300

    task-app App component is now a class instead of a function

--- reactjs/task-app/src/App.js
+++ reactjs/task-app/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import {component} from 'react';
+
 import Overview from './components/Overview';
 
-function App() {
-  return (
-    <div>
-      <Overview people={['pepe','juancito','bolainas']}/>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <input type="text"/>
+          <button type="button" name="add-task-btn">ADD</button>
+        </div>
+        <Overview people={['pepe','juancito','bolainas']}/>
+      </div>
+    );
+  }
 }
 
 export default App;
